610406171518fe95de517ba2e329b7e29f206df0
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-07 18:24:11 +0530

    Completed the loging screen styles

--- src/styles/Loading.styles.tsx
+++ src/styles/Loading.styles.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#FFF4EA', 
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -12,6 +12,11 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontWeight: 'bold',
     color: '#333333',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
   },
 });
 
