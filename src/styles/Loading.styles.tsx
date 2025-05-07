7ecc30f345ef973404ae71514337fc228c58da6b
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-07 16:16:51 +0530

    Initiated the loading page styles

---
+++ src/styles/Loading.styles.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+});
+
+export default styles;
