25c6be92792c4189c7cc039a9d98106f48a01afa
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-07 18:59:52 +0530

    Add loading screen with ActivityIndicator and logo, navigate to Home after 3 seconds

--- src/screens/Loading.screen.tsx
+++ src/screens/Loading.screen.tsx
@@ -1,9 +1,23 @@
-import { View,ActivityIndicator,Image } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, ActivityIndicator, Image } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import styles from '../styles/Loading.styles';
+import { RootStackParamList } from '../types/navigation';
 
 
 const LoadingScreen = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
+  useFocusEffect(
+    useCallback(() => {
+      const timer = setTimeout(() => {
+        navigation.navigate('Home');
+      }, 3000);
+
+      return () => clearTimeout(timer); 
+    }, [navigation])
+  );
 
   return (
     <View style={styles.container}>
