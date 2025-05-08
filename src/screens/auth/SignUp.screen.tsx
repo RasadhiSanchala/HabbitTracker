7e5c92390eef487d861d46f7e7881d89ae182899
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-08 14:37:29 +0530

    Changed the path of validateUser

--- src/screens/auth/SignUp.screen.tsx
+++ src/screens/auth/SignUp.screen.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert } from 'react-native';
-import { storeUser, getUser } from '../../utils/auth/storage';
+import { storeUser, getUser } from '../../utils/Authstorage';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types/navigation';
