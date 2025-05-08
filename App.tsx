020a43e3f471f874f0649261d4003fb2098702bd
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-08 07:36:07 +0530

    set up root NavigationContainer with AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
-// App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoadingScreen from './src/screens/Loading.screen';
-import HomeScreen from './src/screens/Home.screen'; // ✅ Import your actual Home screen
-
-const Stack = createNativeStackNavigator();
+import AppNavigator from './src/navigation/AppNavigator'; 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Loading" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Loading" component={LoadingScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 };
