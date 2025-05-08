e9699e1cd2c64bac81bd223491ea9e118dc2d63a
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-08 07:40:38 +0530

    imported the SignIn screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoadingScreen from '../screens/Loading.screen';
 import Home from '../screens/Home.screen';
+import SignIn from '../screens/auth/SignIn.screen';
 import { RootStackParamList } from '../types/navigation'; 
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -11,6 +12,7 @@ const AppNavigator = () => {
     <Stack.Navigator initialRouteName="Loading" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Loading" component={LoadingScreen} />
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="SignIn" component={SignIn} />
     </Stack.Navigator>
   );
 };
