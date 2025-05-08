bfd56d863ec3415dcf3204560a1790a1ad76f99d
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-08 12:43:57 +0530

    Added the function for checking for user validation

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,7 +4,7 @@ export const storeUser = async (user: object) => {
   try {
     await AsyncStorage.setItem('user', JSON.stringify(user));
   } catch (e) {
-    console.log('Saving error:', e);
+    console.log('Saving error', e);
     throw e;
   }
 };
@@ -14,7 +14,20 @@ export const getUser = async () => {
     const jsonValue = await AsyncStorage.getItem('user');
     return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (e) {
-    console.log('Loading error:', e);
+    console.log('Loading error', e);
     return null;
+  }
+};
+
+export const validateUser = async (email: string, password: string) => {
+  try {
+    const user = await getUser();
+    if (user && user.email === email && user.password === password) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    console.log('Validation error', e);
+    return false;
   }
 };
