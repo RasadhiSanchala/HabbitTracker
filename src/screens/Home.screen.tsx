6451fb00f5e0db816e2bd752b89ea2803b9522a4
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-08 07:41:06 +0530

    Added the button to navigate to the SignIn page

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,11 +1,23 @@
-import { Text, View } from "react-native";
+
+import { Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/navigation'; 
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
 function Home() {
-    return ( 
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>This is Home!</Text>
-        </View>
-     );
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This is Home!</Text>
+      <Button
+        title="Get Started"
+        onPress={() => navigation.navigate('SignIn')}
+      />
+    </View>
+  );
 }
 
 export default Home;
