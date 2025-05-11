1e4914860f10fe09d7fe89933332180b5770f783
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-11 21:49:37 +0530

    Just created the SelectDays screen.but didn't add anything yet.just created for navigation testing purposes.

---
+++ src/screens/SelectDays.screen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SelectDays() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Select Days Screen - You can build this later</Text>
+    </View>
+  );
+}
