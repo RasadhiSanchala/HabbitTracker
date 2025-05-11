0d56d0a7dace643ece9770e1dad971b3c1850677
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-11 21:47:49 +0530

    Wrap NavigationContainer with HabitProvider to enable global habit state

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator'; 
 import { AuthProvider } from './src/context/AuthContext';
+import {HabitProvider} from './src/context/HabitContext';
 
 const App = () => {
   return (
     <AuthProvider>
+       <HabitProvider>
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
+     </HabitProvider>
    </AuthProvider>
   );
 };
