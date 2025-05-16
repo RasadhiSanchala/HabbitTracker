30cbd28adefb6ee23cb131af4596535b0c29e726
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-16 15:21:58 +0530

    wrap app with HabitProvider to enable habit context and storage

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator'; 
+import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
-import {HabitProvider} from './src/context/HabitContext';
+import { HabitProvider } from './src/context/HabitContext';
 
 const App = () => {
   return (
     <AuthProvider>
-       <HabitProvider>
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-     </HabitProvider>
-   </AuthProvider>
+      <HabitProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </HabitProvider>
+    </AuthProvider>
   );
 };
 
