60e18b5e6dc8d96efd7ebbe47d04b1809c1edf56
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-16 14:57:32 +0530

    Add onSelectDays callback param to SelectDays route in RootStackParamList

--- src/types/navigation.tsx
+++ src/types/navigation.tsx
@@ -1,10 +1,11 @@
 export type RootStackParamList = {
-    Loading: undefined;
-    Home:undefined;
-    SignIn: undefined;
-    SignUp: undefined;
-    Dashboard: undefined;
-    AddHabbit: undefined;
-    SelectDays: undefined;
+  Loading: undefined;
+  Home: undefined;
+  SignIn: undefined;
+  SignUp: undefined;
+  Dashboard: undefined;
+  AddHabbit: undefined;
+  SelectDays: {
+    onSelectDays: (days: string[]) => void;
   };
-  
+};
