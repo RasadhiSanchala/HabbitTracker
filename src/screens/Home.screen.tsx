5c55537e92efa8b64c9b80d762e62393d254b6c9
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-05-22 22:58:56 +0530

    Imported the bottomnav bar

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -3,18 +3,36 @@ import { Text, View, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/navigation'; 
+import BottomNavBar from '../components/BottomNavBar';
 
 type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
 function Home() {
   const navigation = useNavigation<HomeScreenNavigationProp>();
 
+    const handleHome = () => navigation.navigate('Home');
+  const handleDashboard = () => navigation.navigate('Dashboard');
+  const handleAdd = () => navigation.navigate('AddHabbit');
+  const handleProgress = () => navigation.navigate('Home');
+  const handleLogout = () => {
+    navigation.navigate('Home'); 
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1 }}>
+    <View style={{flex: 1 ,justifyContent: 'center', alignItems: 'center' }}>
       <Text>This is Home!</Text>
       <Button
         title="Get Started"
         onPress={() => navigation.navigate('SignIn')}
+      />
+    </View>
+        <BottomNavBar
+        onHome={handleHome}
+        onDashboard={handleDashboard}
+        onAdd={handleAdd}
+        onProgress={handleProgress}
+        onLogout={handleLogout}
       />
     </View>
   );
